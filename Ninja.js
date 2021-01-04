6f51e169bbb7ef1833977ee4b5fef56eca1c52e9
Javier Rivera <Sunnuke@users.noreply.github.com>
2021-01-04 13:21:25 -0800

    Added some methods to the Ninja class

--- Ninja.js
+++ Ninja.js
@@ -1,8 +1,24 @@
 class Ninja {
+    // ATTRIBUTES
     constructor(name) {
         this.name = name;
         this.health = 100;
         this.speed = 3;
         this.strength =3;
+    }
+    // METHODS
+    sayName() {
+        console.log(this.name);
+    }
+    showStats() {
+        console.log(
+            "Name: " + this.name + "\n" +
+            "Strength: " + this.strength + "\n" +
+            "Speed: " + this.speed + "\n" +
+            "Health: " + this.health
+        );
+    }
+    drinkSake() {
+        this.health += 10;
     }
 }
